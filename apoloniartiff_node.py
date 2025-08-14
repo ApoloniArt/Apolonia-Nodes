2503fc7b57cd6712a183ce239e5ea179d42582aa
Apolonia <twogunrocky@yahoo.co.uk>
2025-08-14 09:50:24 +0100

    Add files via upload

---
+++ apoloniartiff_node.py
@@ -0,0 +1,107 @@
+from comfy.cli_args import args
+import folder_paths
+import json
+import numpy
+import os
+from PIL import Image, ExifTags
+from PIL.PngImagePlugin import PngInfo
+
+class ApoloniArtiff:
+    def __init__(self):
+        pass
+
+    FILE_TYPE_PNG = "PNG"
+    FILE_TYPE_JPEG = "JPEG"
+    FILE_TYPE_WEBP_LOSSLESS = "WEBP (lossless)"
+    FILE_TYPE_WEBP_LOSSY = "WEBP (lossy)"
+    FILE_TYPE_TIFF = "TIFF (lossless)"
+    RETURN_TYPES = ()
+    FUNCTION = "save_images"
+    OUTPUT_NODE = True
+    CATEGORY = "image"
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "images": ("IMAGE", ),
+                "filename_prefix": ("STRING", {"default": "ComfyUI"}),
+                "file_type": ([s.FILE_TYPE_PNG, s.FILE_TYPE_JPEG, s.FILE_TYPE_WEBP_LOSSLESS, s.FILE_TYPE_WEBP_LOSSY, s.FILE_TYPE_TIFF], ),
+                "remove_metadata": ("BOOLEAN", {"default": False}),
+            },
+            "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
+        }
+
+    def save_images(self, images, filename_prefix="ComfyUI", file_type=FILE_TYPE_PNG, remove_metadata=False, prompt=None, extra_pnginfo=None):
+        output_dir = folder_paths.get_output_directory()
+        full_output_folder, filename, counter, subfolder, _ = folder_paths.get_save_image_path(filename_prefix, output_dir, images[0].shape[1], images[0].shape[0])
+        extension = {
+            self.FILE_TYPE_PNG: "png",
+            self.FILE_TYPE_JPEG: "jpg",
+            self.FILE_TYPE_WEBP_LOSSLESS: "webp",
+            self.FILE_TYPE_WEBP_LOSSY: "webp",
+            self.FILE_TYPE_TIFF: "tiff",
+        }.get(file_type, "png")
+
+        results = []
+        for image in images:
+            array = 255. * image.cpu().numpy()
+            img = Image.fromarray(numpy.clip(array, 0, 255).astype(numpy.uint8))
+
+            kwargs = dict()
+            if extension == "png":
+                kwargs["compress_level"] = 4
+                if not remove_metadata and not args.disable_metadata:
+                    metadata = PngInfo()
+                    if prompt is not None:
+                        metadata.add_text("prompt", json.dumps(prompt))
+                    if extra_pnginfo is not None:
+                        for x in extra_pnginfo:
+                            metadata.add_text(x, json.dumps(extra_pnginfo[x]))
+                    kwargs["pnginfo"] = metadata
+            elif extension == "tiff":
+                kwargs["compression"] = "tiff_lzw"  # LZW compression for lossless TIFF
+                if not remove_metadata and not args.disable_metadata:
+                    exif = img.getexif()
+                    metadata = {}
+                    if prompt is not None:
+                        metadata["prompt"] = prompt
+                    if extra_pnginfo is not None:
+                        metadata.update(extra_pnginfo)
+                    exif[ExifTags.Base.UserComment] = json.dumps(metadata)
+                    kwargs["exif"] = exif
+            else:
+                if file_type == self.FILE_TYPE_WEBP_LOSSLESS:
+                    kwargs["lossless"] = True
+                else:
+                    kwargs["quality"] = 90
+                if not remove_metadata and not args.disable_metadata:
+                    metadata = {}
+                    if prompt is not None:
+                        metadata["prompt"] = prompt
+                    if extra_pnginfo is not None:
+                        metadata.update(extra_pnginfo)
+                    exif = img.getexif()
+                    exif[ExifTags.Base.UserComment] = json.dumps(metadata)
+                    kwargs["exif"] = exif.tobytes()
+
+            file = f"{filename}_{counter:05}_.{extension}"
+            img.save(os.path.join(full_output_folder, file), **kwargs)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": "output",
+            })
+            counter += 1
+
+        return { "ui": { "images": results } }
+
+NODE_CLASS_MAPPINGS = {
+    "ApoloniArtiff": ApoloniArtiff
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ApoloniArtiff": "ApoloniArtiff"
+}
+
+WEB_DIRECTORY = "web"
